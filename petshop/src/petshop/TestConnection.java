07164da30ccde401258754ed768a3ea29af22fda
MarcoAntonioFlor <marco.antonio@mercadolivre.com>
2020-08-22 23:48:15 -0300

    Commit de teste do git
    Estou adicionando um comentário na sua classe para vermos qual é o comportamento no github

--- petshop/src/petshop/TestConnection.java
+++ petshop/src/petshop/TestConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class TestConnection {
-	
+	//Ensinando sobre git
 	public static void main(String[] args) throws SQLException {
         Connection conexao = DriverManager.getConnection("jdbc:mysql://localhost/cadastro", "root", "root");
         System.out.println("Conectado!");
